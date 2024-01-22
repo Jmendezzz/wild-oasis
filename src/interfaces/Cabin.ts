90c70f46b8335e57386f587bf3e71ee5b1dcdbb4
Juan Gerardo <jmendez1025@cue.edu.co>
2024-01-22 11:29:55 -0500

    Initial commit, base of the project

---
+++ src/interfaces/Cabin.ts
@@ -0,0 +1,9 @@
+
+export interface Cabin{
+    name:string,
+    maxCapacity:number,
+    regularPrice:number,
+    discount:number,
+    image:string,
+    description:string
+}
---
+++ src/ui/Heading.tsx
@@ -0,0 +1,32 @@
+import styled, { css } from 'styled-components';
+
+interface Props {
+  type?: string;
+}
+
+//We use as in order to change the semantic tag in HTML so this will be render
+// like: <h1> <h1/> depending on the value that has the prop 'as'.
+
+const Heading = styled.h1<Props>`
+  ${(props) =>
+    props.as === 'h1' &&
+    css`
+      font-size: 3rem;
+    `}
+
+  ${(props) =>
+    props.as === 'h2' &&
+    css`
+      font-size: 2rem;
+    `}
+
+  ${(props) =>
+    props.as=== 'h3' &&
+    css`
+      font-size: 1rem;
+    `}
+
+  font-weight:600;
+`;
+
+export default Heading;
